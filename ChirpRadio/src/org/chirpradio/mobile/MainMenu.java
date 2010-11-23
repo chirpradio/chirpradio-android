21d2e99c888451b664cf414740532538ecc22157
Jim Benton <jim@autonomousmachine.com>
2010-11-23 00:01:01 -0600

    Added some buttons to the main menu

--- ChirpRadio/src/org/chirpradio/mobile/MainMenu.java
+++ ChirpRadio/src/org/chirpradio/mobile/MainMenu.java
@@ -8,6 +8,6 @@ public class MainMenu extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_menu);
     }
 }
