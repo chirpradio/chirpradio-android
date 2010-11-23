8909a18bb2e0b195b9eaa1e1d22fac68ea34f78c
Jim Benton <jim@autonomousmachine.com>
2010-11-23 00:14:09 -0600

    Adding a click listener to show the Playing activity

--- ChirpRadio/src/org/chirpradio/mobile/MainMenu.java
+++ ChirpRadio/src/org/chirpradio/mobile/MainMenu.java
@@ -3,11 +3,28 @@ package org.chirpradio.mobile;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainMenu extends Activity {
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainMenu extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+        
+        View playingButton = findViewById(R.id.playing_button);
+        playingButton.setOnClickListener(this);
+        
+    }
+    
+    public void onClick(View v) {
+    	switch (v.getId()) {
+    	case R.id.playing_button:
+    		Intent i = new Intent(this, Playing.class);
+    		startActivity(i);
+    		break;
+    	}
     }
 }
---
+++ ChirpRadio/src/org/chirpradio/mobile/Playing.java
@@ -0,0 +1,14 @@
+package org.chirpradio.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Playing extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.playing);
+	}
+	
+}
