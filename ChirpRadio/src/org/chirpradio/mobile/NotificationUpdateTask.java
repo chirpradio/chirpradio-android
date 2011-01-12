72bbc0e7b3411b9b338a1e352539f64574786844
Jim Benton <jim@autonomousmachine.com>
2011-01-11 23:47:09 -0600

    Clean up NotificationUpdateTask a bit

--- ChirpRadio/src/org/chirpradio/mobile/NotificationUpdateTask.java
+++ ChirpRadio/src/org/chirpradio/mobile/NotificationUpdateTask.java
@@ -12,6 +12,8 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.npr.android.news.PlaybackService;
+
 import android.text.Html;
 
 import android.os.AsyncTask;
@@ -23,14 +25,17 @@ import android.content.Context;
 
 public class NotificationUpdateTask extends AsyncTask<Context, Void, JSONObject> {
 
+	private static final String LOG_TAG = NotificationUpdateTask.class.toString();
 	private static final int NOTIFICATION_ID = 1;
+	
 	private Context context;
 	
     protected JSONObject doInBackground(Context... contexts) {
-    	Context context = contexts[0];
-    	this.context = context;
+    	this.context = contexts[0];
+    	
         HttpGet get = new HttpGet("http://chirpradio.org/json");
         DefaultHttpClient client = new DefaultHttpClient();
+        
         try {
 			HttpResponse response = client.execute(get);
 			String responseBody = EntityUtils.toString(response.getEntity());
@@ -38,27 +43,24 @@ public class NotificationUpdateTask extends AsyncTask<Context, Void, JSONObject>
 			// JSON response contains invalid 'comments', so strip them out
 			responseBody = responseBody.replaceFirst("<!-- cached copy -->", "");
 			responseBody = responseBody.replaceFirst("<!-- end cached copy -->", "");
+			responseBody = responseBody.replaceFirst("<!-- fresh copy -->", "");
+			responseBody = responseBody.replaceFirst("<!-- end fresh copy -->", "");
 			
 			// Decode HTML entities
 			responseBody = Html.fromHtml(responseBody).toString();
 			
 			try {
-				JSONObject object = (JSONObject) new JSONTokener(responseBody).nextValue();
-				Log.i("artist", object.getString("artist"));
-				Log.i("song-title", object.getString("song-title"));
-				Log.i("album", object.getString("album"));
-				return object;
+				return (JSONObject) new JSONTokener(responseBody).nextValue();
 			} catch (JSONException e) {
-				// error parsing JSON
-				e.printStackTrace();
+		        Log.e(LOG_TAG, "", e);
 			}
 			
 		} catch (ClientProtocolException e) {
-			// in case of a problem or the connection was aborted
-			e.printStackTrace();
+			// a problem connecting or the connection was aborted
+	        Log.e(LOG_TAG, "", e);
 		} catch (IOException e) {
-			// in case of an http protocol error
-			e.printStackTrace();
+			// an http protocol error
+	        Log.e(LOG_TAG, "", e);
 		}
 		return new JSONObject();
     }
@@ -67,29 +69,26 @@ public class NotificationUpdateTask extends AsyncTask<Context, Void, JSONObject>
 	    int icon = R.drawable.icon;
 	    long when = System.currentTimeMillis();
 	    
-	    String artist;
 		try {
-			artist = data.getString("artist");
-		    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		    
-		    Notification notification = new Notification(icon, artist, when);
-		    notification.flags = Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
-		    
-		    Context c = context.getApplicationContext();
+			String artist = data.getString("artist");
+
 		    CharSequence title = context.getString(R.string.app_name);
-		    Intent notificationIntent;
-		    notificationIntent = new Intent(c, MainMenu.class);
+		    Intent notificationIntent = new Intent(context, MainMenu.class);
 		    notificationIntent.setAction(Intent.ACTION_VIEW);
 		    notificationIntent.addCategory(Intent.CATEGORY_DEFAULT);
 		    notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		    PendingIntent contentIntent = PendingIntent.getActivity(c, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-		    notification.setLatestEventInfo(c, title, artist, contentIntent);
+		    
+		    PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+		    
+		    Notification notification = new Notification(icon, artist, when);
+		    notification.flags = Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+		    notification.setLatestEventInfo(context, title, artist, contentIntent);
+		    
+		    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		    notificationManager.notify(NOTIFICATION_ID, notification);
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	        Log.e(LOG_TAG, "", e);
 		}
 
     }
-	
 }
